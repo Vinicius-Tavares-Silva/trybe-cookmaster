a158d7e19e7b26871f83a83e498a04ea673ae4b3
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-12-04 19:37:55 -0300

    Requisito 6

--- controllers/recipes/get.js
+++ controllers/recipes/get.js
@@ -5,11 +5,7 @@ module.exports = async (req, res, next) => {
     const { id } = req.params;
     const object = await model.findById(id);
     if (!object) {
-      return res.status(422).send({ err: {
-        code: 'invalid_data',
-        message: 'Wrong id format',
-      },
-      });
+      return res.status(404).send({ message: 'recipe not found' });
     }
 
     return res.status(200).send(object);
